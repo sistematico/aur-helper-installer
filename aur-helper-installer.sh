ebb7e5ff7db518c897f82fd90f87b05e260cbbc3
José Augusto <cmdt.jaugusto@gmail.com>
2018-05-07 15:34:48 -0300

    [MOD] mod the code to add more aur helpers

--- aur-helper-installer.sh
+++ aur-helper-installer.sh
@@ -10,12 +10,39 @@
 ##################################################
 
 function main() {
-	
+    clear
+    read -p "
+    --- Select an aur helper ---
+
+    [1] - yay
+    [2] - yaourt
+    [3] - trizen
+    " input;
+
+    if [[ $input -eq 1 ]]; then
+        clear;
+        yay_install;
+    elif [[ $input -eq 2 ]]; then
+        clear;
+        yaourt_install;
+    elif [[ $input -eq 3 ]]; then
+        clear;
+        trizen_install;
+    else
+        echo "Wrong input, try again!"
+        main;
+    fi
+}
+
+function yay_install() {
+    echo "Installing yay..."
+    echo "Yet another yogurt. Pacman wrapper and AUR helper written in go."
+
     curl -o yay.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
     tar zxvf yay.tar.gz
     rm yay.tar.gz
     cd yay
-    makepkg -csi --noconfirm
+    makepkg -csi --noconfirm && echo 'DONE!'
 }
 
 main;
